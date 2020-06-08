57b433edcb791d4d91d52c0166b9296515e4daea
BitterBlue22 <telisax@gmail.com>
2020-06-08 11:19:09 +0100

    adds upvote and downvote functionality

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
-const RestaurantCard = () => {
-  return (
-    <article className="restaurant-card">
-      <button className="btn-red">RED</button>
-      <button className="btn-green">GREEN</button>
-    </article>
-  );
-};
+class RestaurantCard extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleUpvote = () => {
+    this.setState({ count: this.state.count + 1 });
+    console.log(this.state.count);
+  };
+  handleDownvote = () => {
+    this.setState({ count: this.state.count - 1 });
+    console.log(this.state.count);
+  };
+  render() {
+    return (
+      <article className="restaurant-card">
+        <button className="btn-red" onClick={this.handleDownvote}>
+          RED
+        </button>
+        <button className="btn-green" onClick={this.handleUpvote}>
+          GREEN
+        </button>
+      </article>
+    );
+  }
+}
 
 export default RestaurantCard;
