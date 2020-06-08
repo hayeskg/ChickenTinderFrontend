e36ef96cbcb759ace6889247ba6a58cfb871a9f2
BitterBlue22 <telisax@gmail.com>
2020-06-08 11:08:28 +0100

    added restaurant-card

---
+++ src/components/RestaurantCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RestaurantCard = () => {
+  return (
+    <article className="restaurant-card">
+      <button className="btn-red">RED</button>
+      <button className="btn-green">GREEN</button>
+    </article>
+  );
+};
+
+export default RestaurantCard;
