50846ab7cc8d5db9c4f85b340b3eca79f291ca0a
Lajanke <l.j.kenny@hotmail.com>
2020-06-09 15:44:21 +0100

    Adds event creation form component

---
+++ src/components/EventCreationForm.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class EventCreator extends React.Component {
+
+    state = {};
+
+    render() {
+
+        return (
+            <p>EventForm</p>
+        );
+    };
+};
+
+export default EventCreator;
