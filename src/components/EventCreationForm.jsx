2580ca9e697e94fe169924fa7a2b3f86926a0435
Lajanke <l.j.kenny@hotmail.com>
2020-06-09 16:03:09 +0100

    provides a basic form layout.

--- src/components/EventCreationForm.jsx
+++ src/components/EventCreationForm.jsx
@@ -1,13 +1,42 @@
 import React from "react";
 
 class EventCreator extends React.Component {
+    state = {
+        eventName: "",
+        location: "",
+        radius: "",
+    };
 
-    state = {};
+    handleInputChange = (event) => {
+        
+        const { value, name } = event.target
+        this.setState({
+            [name]: value,
+        })
+        console.log(this.state)
+    }
 
     render() {
-
+        const { eventName, location, radius } = this.state
         return (
-            <p>EventForm</p>
+            <form onSubmit={this.handleSubmitForm} className='eventForm'>
+                <label>Event Name:
+            <input type="text" name='eventName' required='required' onChange={this.handleInputChange} placeholder='Event name here...' value={eventName} />
+                </label>
+                <label >Location:
+            <input type="text" name='location' required='required' onChange={this.handleInputChange} placeholder='Location here...' value={location} />
+                </label>
+                <label >Radius:
+            <select className='select' name='topicradius' onChange={this.handleInputChange} value={radius}>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">3</option>
+                        <option value="5">3</option>
+                    </select>
+                </label>
+                <button className='postButton'>Create Event</button>
+            </form>
         );
     };
 };
