6afd06c0e99e08b8532309fbdf0a0ea8c2529c23
BitterBlue22 <telisax@gmail.com>
2020-06-10 11:50:49 +0100

    updates minor styling

--- src/components/EndOfList.jsx
+++ src/components/EndOfList.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+
+const StyledSection = styled.section`
+  background-colour: white;
+  border-style: groove;
+  border-colour: black;
+  padding: 20px;
+  margin 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+ align-items: center;
+  flex-direction: column;
+`;
+
+const StyledReturnButton = styled.button`
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+  font-size: 20px;
+`;
 
 const EndOfList = () => {
-  return (
-    <section>
-      THE END
-    </section>
-  );
+  return <StyledSection>THE END</StyledSection>;
 };
 
 export default EndOfList;
