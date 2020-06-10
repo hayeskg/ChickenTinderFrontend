f712cfe3c7a1fed958e78820cfa63c61218d58cd
BitterBlue22 <telisax@gmail.com>
2020-06-10 11:39:55 +0100

    updates header

--- src/components/re-usable/Header.jsx
+++ src/components/re-usable/Header.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
+const StyledHeader = styled.header`
+  background-color: black;
+  color: white;
+  padding: 10px;
+  display: flex;
+  justify-content: space-around;
+`;
 const Header = () => {
   return (
-    <section>
+    <StyledHeader>
       <h1>Chicken Tinder</h1>
-    </section>
+    </StyledHeader>
   );
 };
 
