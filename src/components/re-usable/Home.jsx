b3b8c2fb1c2b3d1894cf9079e366fc71109b8635
BitterBlue22 <telisax@gmail.com>
2020-06-11 13:38:29 +0100

    several style updates

--- src/components/re-usable/Home.jsx
+++ src/components/re-usable/Home.jsx
@@ -4,15 +4,15 @@ import { Link } from "@reach/router";
 
 const Home = () => {
   return (
-    <section>
+    <section className="home-wrapper">
       <Link to="/event-creation">
-        <button>
-          <span>Create Event</span>
+        <button className="event-button">
+          <span className="event-button-span">Create Event</span>
         </button>
       </Link>
       <Link to="/swipe">
-        <button>
-          <span> Join Event</span>
+        <button className="event-button">
+          <span className="event-button-span"> Join Event</span>
         </button>
       </Link>
     </section>
