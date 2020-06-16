cd70a30777e8a939e097776c942c2b1d51660435
Lajanke <l.j.kenny@hotmail.com>
2020-06-16 13:20:56 +0100

    adds restaurant info, external link to website

--- src/components/WinnerDisplayer.jsx
+++ src/components/WinnerDisplayer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const WinnerDisplayer = ({data}) => {
+const WinnerDisplayer = ({ data }) => {
 
     const { id,
         eventId,
@@ -15,37 +15,39 @@ const WinnerDisplayer = ({data}) => {
         website,
         address,
         cuisine,
-        dietRestrictions} = data.winner
+        dietRestrictions } = data.winner
 
-      return (
+    return (
         <article>
             <h2>YOUR MATCH</h2>
             <div className="winner-card">
-        <section className="restaurant-card">
-          <h2>{name}</h2>
-          <p>{cuisine[0]}</p>
-            
-            
-          <p>
-            <FontAwesomeIcon icon="dollar-sign" className="icon" /> {price}
-          </p>
-          <p>
-            <FontAwesomeIcon icon="star" className="icon" />
-            {rating}
-          </p>
-          <p>{description}</p>
-          {/*<p>
-            <FontAwesomeIcon icon="map-marker-alt" className="icon" />
-            {location_string}
-          </p>*/}
-        </section>
-        <img src={photo} alt={name} className="restaurant-image" />
-        <p>Address: {address}</p>
-        <p>{website}</p>
-        <p>{phone}</p>
-        </div>
-      </article>   
-      )
+                <section className="restaurant-card">
+                    <h2>{name}</h2>
+                    <p>{cuisine[0]}</p>
+                    <p>{dietRestrictions.join(", ")}</p>
+                    <div className="price-rating">
+                    <p>
+                        <FontAwesomeIcon icon="dollar-sign" className="icon" /> 
+                        {price}
+                    </p>
+                    <p>
+                        <FontAwesomeIcon icon="star" className="icon" />
+                        {rating}
+                    </p>
+                    </div>
+                    <p>{description}</p>
+                </section>
+                <img src={photo} alt={name} className="restaurant-image" />
+                <p>
+                    <FontAwesomeIcon icon="map-marker-alt" className="icon" />
+                    {address}</p>
+                <a href={website}>
+                    <button>{name} website</button>
+                </a>
+                <p>Phone Number: {phone}</p>
+            </div>
+        </article>
+    )
 }
 
 export default WinnerDisplayer;
