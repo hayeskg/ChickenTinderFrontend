b460afcf9ce42457020cf3c0da90c1f218a230b5
Lajanke <l.j.kenny@hotmail.com>
2020-06-17 17:01:35 +0100

    adds get user query

---
+++ src/queries/GetUsers.jsx
@@ -0,0 +1,34 @@
+import Loader from "../components/re-usable/Loader";
+import React from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import EventCreationForm from "../components/EventCreationForm"
+
+const getUsers = gql`
+query  {
+  users 
+   {
+     id
+    uid
+    email
+  }
+  }
+  `;
+
+  const GetUsers = ({ id }) => {
+
+    return (
+      <div>
+        <Query query={getUsers}>
+          {({ loading, error, data }) => {
+            if (loading) return <Loader />;
+            if (error) console.log(error);
+            console.log(data)
+            return <EventCreationForm query={data} eventId={id}/>;
+          }}
+        </Query>
+      </div>
+    );
+  };
+
+  export default GetUsers;
