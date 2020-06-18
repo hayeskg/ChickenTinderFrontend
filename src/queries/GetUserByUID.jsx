b98ff9e110e962c77b0ac5e95becb1d32f7e05be
Lajanke <l.j.kenny@hotmail.com>
2020-06-18 10:24:10 +0100

    adds get user by uid query.

---
+++ src/queries/GetUserByUID.jsx
@@ -0,0 +1,35 @@
+import Loader from "../components/re-usable/Loader";
+import React from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import Home from "../components/re-usable/Home";
+
+const getUserByUID = gql`
+query ($uid: String!){
+    userByUID (
+      uid: $uid
+    ){
+      id
+        email
+      uid
+    }
+    }
+    `;
+
+  const GetUserByUID = (uid) => {
+    
+    return (
+      <div>
+        <Query query={getUserByUID} variables={uid}>
+          {({ loading, error, data }) => {
+            if (loading) return <Loader />;
+            if (error) console.log(error);
+
+            return <Home query={data} />;
+          }}
+        </Query>
+      </div>
+    );
+  };
+
+  export default GetUserByUID;
