6808403307b03e1c7259450b676c905b8ce50dc3
BitterBlue22 <telisax@gmail.com>
2020-06-18 16:26:26 +0100

    adds error handling to GET query components

--- src/queries/GetUsers.jsx
+++ src/queries/GetUsers.jsx
@@ -2,33 +2,30 @@ import Loader from "../components/re-usable/Loader";
 import React from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
-import EventCreationForm from "../components/EventCreationForm"
+import EventCreationForm from "../components/EventCreationForm";
 
 const getUsers = gql`
-query  {
-  users 
-   {
-     id
-    uid
-    email
+  query {
+    users {
+      id
+      uid
+      email
+    }
   }
-  }
-  `;
+`;
 
-  const GetUsers = ({ id }) => {
+const GetUsers = ({ id }) => {
+  return (
+    <div>
+      <Query query={getUsers}>
+        {({ loading, error, data }) => {
+          if (loading) return <Loader />;
+          if (error) return <ErrorDisplayer msg={error} />;
+          return <EventCreationForm query={data} eventId={id} />;
+        }}
+      </Query>
+    </div>
+  );
+};
 
-    return (
-      <div>
-        <Query query={getUsers}>
-          {({ loading, error, data }) => {
-            if (loading) return <Loader />;
-            if (error) console.log(error);
-            console.log(data)
-            return <EventCreationForm query={data} eventId={id}/>;
-          }}
-        </Query>
-      </div>
-    );
-  };
-
-  export default GetUsers;
+export default GetUsers;
