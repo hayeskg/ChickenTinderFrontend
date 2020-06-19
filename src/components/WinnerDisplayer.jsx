413fb88188ba5aaa44e6b3a386c16f73b82d9efd
BitterBlue22 <telisax@gmail.com>
2020-06-19 10:25:12 +0100

    updates broken links

--- src/components/WinnerDisplayer.jsx
+++ src/components/WinnerDisplayer.jsx
@@ -3,60 +3,65 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "@reach/router";
 
 const WinnerDisplayer = ({ data }) => {
+  const {
+    id,
+    eventId,
+    name,
+    description,
+    photo,
+    price,
+    ranking,
+    rating,
+    phone,
+    website,
+    address,
+    cuisine,
+    dietRestrictions,
+  } = data;
 
-    
-    const {
-        eventId,
-        name,
-        description,
-        photo,
-        price,
-        ranking,
-        rating,
-        phone,
-        website,
-        address,
-        cuisine,
-        dietRestrictions } = data;
+  const firstCuisine =
+    cuisine && cuisine.length > 0 ? cuisine[0] : "Not available";
 
-    const firstCuisine = cuisine && cuisine.length > 0 ? cuisine[0] : 'Not available';
-    
-    return (
-        <article>
-            <h2>YOUR MATCH</h2>
-            <div className="winner-card">
-                <section className="restaurant-card"   event={eventId}
-        mongoId={id}
-        ranking={ranking}>
-                    <h2>{name}</h2>
-                    <p>{firstCuisine}</p>
-                    <p>{dietRestrictions.join(", ")}</p>
-                    <div className="price-rating">
-                        <p>
-                            <FontAwesomeIcon icon="dollar-sign" className="icon" />
-                            {price}
-                        </p>
-                        <p>
-                            <FontAwesomeIcon icon="star" className="icon" />
-                            {rating}
-                        </p>
-                    </div>
-                    <p>{description}</p>
-                </section>
-                <img src={photo} alt={name} className="restaurant-image" />
-                <p>
-                    <FontAwesomeIcon icon="map-marker-alt" className="icon" />
-                    {address}</p>
-                <a href={website}>
-                    <button>{name} website</button>
-                </a>
-                <p>Phone Number: {phone}</p>
-            </div>
-            <Link to="/">
-                <button>Home</button>
-            </Link>
-        </article>
-    )
-}
+  return (
+    <article>
+      <h2>YOUR MATCH</h2>
+      <div className="winner-card">
+        <section
+          className="restaurant-card"
+          event={eventId}
+          mongoId={id}
+          ranking={ranking}
+        >
+          <h2>{name}</h2>
+          <p>{firstCuisine}</p>
+          <p>{dietRestrictions.join(", ")}</p>
+          <div className="price-rating">
+            <p>
+              <FontAwesomeIcon icon="dollar-sign" className="icon" />
+              {price}
+            </p>
+            <p>
+              <FontAwesomeIcon icon="star" className="icon" />
+              {rating}
+            </p>
+          </div>
+          <p>{description}</p>
+        </section>
+        <img src={photo} alt={name} className="restaurant-image" />
+        <p>
+          <FontAwesomeIcon icon="map-marker-alt" className="icon" />
+          {address}
+        </p>
+        <a href={website}>
+          <button>{name} website</button>
+        </a>
+        <p>Phone Number: {phone}</p>
+      </div>
+      <Link to="/">
+        <button>Home</button>
+      </Link>
+    </article>
+  );
+};
 
 export default WinnerDisplayer;
