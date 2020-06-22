50f2883fda3748589b60dc9382863ce96680b32a
BitterBlue22 <telisax@gmail.com>
2020-06-22 11:56:54 +0100

    updates UpdateUserInfo styling

--- src/components/re-usable/UpdateUserInfo.jsx
+++ src/components/re-usable/UpdateUserInfo.jsx
@@ -4,6 +4,8 @@ import { updateUsername } from "../../queries/UpdateUser.jsx";
 import Loader from "./Loader.jsx";
 import ErrorDisplayer from "./ErrorDisplayer.jsx";
 import fire from "../../fireAuth.js";
+import { Button, TextField } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 
 const UpdateUserInfo = ({ user: { uid } }) => {
   const [username, setUsername] = React.useState("");
@@ -28,23 +30,48 @@ const UpdateUserInfo = ({ user: { uid } }) => {
   }
 
   return (
-    <form>
-      <label>
-        <h4>Username</h4>
-        <input
-          required
-          type="text"
-          onChange={(event) => setUsername(event.target.value)}
-        />
-      </label>
-      <label>
-        <h4>Image URL</h4>
-        <input type="text" onChange={(event) => setPhoto(event.target.value)} />
-      </label>
-      <button onClick={handleSubmit}>Submit</button>
-      {loading && <Loader />}
-      {updateError && <ErrorDisplayer msg={updateError} />}
-    </form>
+    <Grid container justify="center">
+      <form>
+        <label id="updateUser">
+          <h4>Username</h4>
+          <TextField
+            fullWidth
+            required
+            margin="normal"
+            variant="outlined"
+            label="Username"
+            type="text"
+            name="username"
+            placeholder="username"
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </label>
+        <label>
+          <h4>Image URL</h4>
+          <TextField
+            fullWidth
+            margin="normal"
+            variant="outlined"
+            label="ImageURL"
+            type="text"
+            name="image"
+            onChange={(event) => setPhoto(event.target.value)}
+          />
+        </label>
+
+        <Button
+          id="userUpdateButton"
+          size="large"
+          color="primary"
+          variant="contained"
+          onClick={handleSubmit}
+        >
+          Submit
+        </Button>
+        {loading && <Loader />}
+        {updateError && <ErrorDisplayer msg={updateError} />}
+      </form>
+    </Grid>
   );
 };
 
