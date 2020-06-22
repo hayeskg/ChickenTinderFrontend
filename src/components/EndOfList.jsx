110f051d27493d7b68e1ad9f6d99d94c236c4b8b
jordan <contact@creativecuckoo.com>
2020-06-22 13:00:26 +0100

    styles end of voting button

--- src/components/EndOfList.jsx
+++ src/components/EndOfList.jsx
@@ -1,41 +1,17 @@
-import React from "react";
-import styled from "styled-components";
-import { Link } from "@reach/router";
-
-const StyledSection = styled.section`
-  background-color: #EB5E30;
-  border-style: groove;
-  border-color: black;
-  padding: 20px;
-  margin 20px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
- align-items: center;
-  flex-direction: column;
-  border-radius: 5px;
-`;
-
-const StyledReturnButton = styled.button`
-  background-color: #5c8f22;
-  padding: 20px;
-  border-radius: 5px;
-  font-size: 20px;
-  margin: 20px;
-  &:hover {
-    background-color: #fd974f;
-    transition: all 500ms ease;
-  }
-`;
+import React from 'react';
+import { Link } from '@reach/router';
+import { Grid, Button } from '@material-ui/core';
 
 const EndOfList = ({ id }) => {
   return (
-    <StyledSection>
-      THE END
-      <Link to="/">
-        <StyledReturnButton>Back home</StyledReturnButton>
-      </Link>
-    </StyledSection>
+    <Grid container justify="center">
+      <Grid item xs={6}>
+        <h2>Thanks for your votes!</h2>
+        <Button variant="contained" color="primary" size="large">
+          <Link to="/">Home</Link>
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
 
