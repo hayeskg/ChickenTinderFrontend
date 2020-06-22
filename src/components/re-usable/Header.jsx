9ac8082899f2db149c6a0da30034a08c277f0901
BitterBlue22 <telisax@gmail.com>
2020-06-22 11:57:12 +0100

    updates header styling

--- src/components/re-usable/Header.jsx
+++ src/components/re-usable/Header.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import { Link } from '@reach/router';
-import { ReactComponent as Logo } from '../../styling/assets/logo.svg';
+import React from "react";
+import { Link } from "@reach/router";
+import { ReactComponent as Logo } from "../../styling/assets/logo.svg";
 
 const Header = ({ user: { email, photo, username } }) => {
   return (
     <header>
       <Link to="/">
-        <Logo width={'40%'} height={'40%'} alt={'Chicken Tinder Logo'} />
+        <Logo width={"40%"} height={"40%"} alt={"Chicken Tinder Logo"} />
       </Link>
       {email ? (
-        <Link to="/user-profile">
-          <button>Logged in as: {username}</button>
-        </Link>
+        <section className="profile-header">
+          <h4>Logged in: {username || "user"}</h4>
+          <Link to="/user-profile">
+            <button className="profile-button">
+              <img
+                className="friend-photo"
+                src={
+                  photo ||
+                  "https://d29fhpw069ctt2.cloudfront.net/icon/image/120759/preview.svg"
+                }
+                alt="avatar"
+              />
+            </button>
+          </Link>
+        </section>
       ) : (
         <p>Please log in to see your profile or sign up to start swiping!</p>
       )}
